9c3c88a87024a0f45ae92875348982cdf4482bf3
kolpa <kolpackov.yarosl@yandex.ru>
2023-01-23 01:38:25 +0700

    2.2 commit

--- AutoService/Pages/OrderTicketPage.xaml.cs
+++ AutoService/Pages/OrderTicketPage.xaml.cs
@@ -21,14 +21,35 @@ namespace AutoService.Pages
     /// </summary>
     public partial class OrderTicketPage : Page
     {
+        List<Product> productList = new List<Product>();
         public OrderTicketPage(Order currentOrder, List<Product> products)
         {
             InitializeComponent();
+            productList = products; // Передаём в пустой лист данные с прошлой страницы
+            DataContext = currentOrder;//Привязываем контекст к классу
+
+            textPickupPoint.Text = currentOrder.PickupPoint.Addres; //Выводим адрес пункта выдачи
+            var result = "";
+
+            foreach (var pl in productList)
+                result += (result == "" ? "" : ", ") + pl.ProductName.ToString(); //Выводим названия товаров, оформленых в заказе
+            textProductList.Text = result.ToString();
+
+            var total = productList.Sum(x => Convert.ToDouble(x.ProductCost) - Convert.ToDouble(x.ProductCost) * Convert.ToDouble(x.ProductDiscountAmount) / 100.00);
+            textCost.Text = total.ToString() + " рублей"; //Выводим итоговую стоимость заказа
+
+            var discountSum = productList.Sum(x => x.ProductDiscountAmount);
+            textDiscountSum.Text = discountSum.ToString() + "%"; //Выводим сумму скидки заказа
         }
 
         private void btnSaveDocument_Click(object sender, RoutedEventArgs e)
         {
-
+            PrintDialog pd = new PrintDialog();
+            if(pd.ShowDialog() == true)
+            {
+                IDocumentPaginatorSource idp = flowDocument;
+                pd.PrintDocument(idp.DocumentPaginator, Title);
+            }
         }
     }
 }
